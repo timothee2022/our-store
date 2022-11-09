3987cefbab69324a12f6e355b466cbfe533b8f32
Timothee Odushina <hsp20tim@gmail.com>
2022-11-09 06:27:13 +0000

    added additional code

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,9 +1,10 @@
 // import { createStyles } from '@mantine/core';
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from './header';
+// import styles from './header';
 import { connect } from "react-redux";
 import { addToCart } from '../../Redux/Shopping/shopping-actions';
+import { Box } from '@mui/material';
 
 
 const Header = ({ productData, addToCart }) => {
@@ -11,7 +12,9 @@ const Header = ({ productData, addToCart }) => {
   return (
     <div>
       <Link to="/">
-        <h1>OUR STORE</h1>
+        <h1 >
+          OUR STORE
+        </h1>
       </Link>
 
 
@@ -19,33 +22,58 @@ const Header = ({ productData, addToCart }) => {
         <h3>Electronics</h3>
       </Link>
 
-      <div className={styles.product}>
-        <img
-          src='https://images.unsplash.com/photo-1610792516307-ea5acd9c3b00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-          alt='your product'
-        />
-
+      <Box sx={{display: 'flex', flexDirection: 'row', textAlign: 'center', margin: '25px'}}>
         <div>
-          <p>title:</p>
-          <p>This is the description</p>
-          <p>$ 15.00</p>
-        </div>
+          <img
+            src='https://images.unsplash.com/photo-1610792516307-ea5acd9c3b00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+            alt='your product'
+          />
 
-        <div>
-          <Link to={`/product/someID`}>
-            <button>
-              VIEW DETAILS
+          <div>
+            <p>title:</p>
+            <p>This is the description</p>
+            <p>$ 15.00</p>
+          </div>
+
+          <img
+            src='https://images.unsplash.com/photo-1588872657578-7efd1f1555ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+            alt='your product'
+          />
+
+          <div>
+            <p>title:</p>
+            <p>This is the description</p>
+            <p>$ 15.00</p>
+          </div>
+
+          <div>
+            <Link to={`/product/someID`}>
+              <button>
+                VIEW DETAILS
+              </button>
+            </Link>
+            <button onClick={() => addToCart(productData.id)}>
+              ADD TO CART
             </button>
-          </Link>
-          <button onClick={() => addToCart(productData.id)}>
-            ADD TO CART
-          </button>
+          </div>
         </div>
-      </div>
+      </Box>
 
       <Link to="/food">
         <h3>Food</h3>
       </Link>
+
+    
+      <img
+            src='https://images.unsplash.com/photo-1522507806580-0be3530796be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+            alt='your product'
+          />
+
+          <div>
+            <p>title:</p>
+            <p>This is the description</p>
+            <p>$ 15.00</p>
+          </div>
 
       <Link to="/cart">
       </Link>
